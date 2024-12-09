291061c17ff777d218f10f38ddee05b5feaa0c73
Shabbir <s.dawoodjee92@gmail.com>
2024-12-09 08:38:10 -0500

    Final

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,6 +2,17 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    if text_to_analyse == "":
+        emotions = {
+        "anger": None,
+        "disgust": None,
+        "fear": None,
+        "joy": None,
+        "sadness": None,
+        "dominant_emotion": None
+        }
+        return emotions
+
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     body = {
--- server.py
+++ server.py
@@ -1,28 +1,53 @@
-from flask import Flask, request, render_template, jsonify
-from EmotionDetection.emotion_detection import emotion_detector 
+"""
+This module implements a Flask application to detect emotions
+in user-provided text input via an API.
+"""
 
-# Initialize Flask app
+from flask import Flask, jsonify, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
 app = Flask(__name__, static_folder="static", template_folder="templates")
+
 
 @app.route("/")
 def home():
+    """
+    Renders the home page of the application.
+
+    Returns:
+        str: Rendered HTML template for the home page.
+    """
     return render_template("index.html")
+
 
 @app.route("/emotionDetector", methods=["GET"])
 def emotion_detect():
-    statement = request.args.get("textToAnalyze")
-    print(f'statement {statement}')
+    """
+    Detects the emotion of the provided text input.
+
+    Returns:
+        Response: JSON response containing the detected emotion or an error message.
+    """
+    statement = request.args.get("textToAnalyze", "").strip()
+
     if not statement:
-        return jsonify({"error": "Missing 'statement' parameter in the request"}), 400
+        response = {"error": "Invalid text! Please try again!"}
+        return jsonify(response), 400
 
     try:
         response = emotion_detector(statement)
-        
-    except Exception as e:
-        print(e)
-        return jsonify({"error": f"Failed to detect emotion: {str(e)}"}), 500
+        dominant_response = response.get("emotion")
 
-    return jsonify(response)
+        if dominant_response is None:
+            response = {"error": "Invalid text! Please try again!"}
+            return jsonify(response), 400
+
+        return jsonify(response)
+
+    except ValueError as error:
+        return jsonify({"error": f"Failed to detect emotion: {error}"}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+    
